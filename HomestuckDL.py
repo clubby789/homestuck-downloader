9fb21302ed946fd700f430b7e4ef8f6e8fac2071
clubby789 <jamie@hill-daniel.co.uk>
2018-05-06 19:00:20 +0100

    Downloads text
    Now downloads page text to /downloaded/images (dirty solution, won't work for all pages likely).

--- HomestuckDL.py
+++ HomestuckDL.py
@@ -1,5 +1,9 @@
-import urllib.request,requests,os,threading,time,glob
-directories=["/downloaded","/downloaded/images"]
+import urllib.request, requests #File retrieval
+from bs4 import BeautifulSoup #Scraping
+import os,glob #Filesystem work
+import threading #Efficiency
+
+directories=["/downloaded","/downloaded/images","./downloaded/text"]
 imagePath = "https://www.homestuck.com/images/storyfiles/hs2/"
 
 for i in directories:
@@ -8,26 +12,50 @@ for i in directories:
         os.makedirs(i)
 
 def dlPage(pageNum):
+    text = []
     
     padNum = str(pageNum).rjust(5,'0')       
     url = "https://www.homestuck.com/story/"+str(pageNum)
-    imageUrl = imagePath+padNum+".gif"
 
-    if requests.get(imageUrl).status_code == 200:
+    r = s.get("https://www.homestuck.com/story/"+str(pageNum))
+    soup = BeautifulSoup(r.text, 'html.parser')
+    text = []
+    text.append(soup.h2.get_text())
+    try:
+        text.append(soup.p.get_text().replace('\r','\n'))
+    except:
+        print("")
+    text.append(soup.find(href="/story/"+str(pageNum+1)).get_text())
+    fileName = os.path.join(directories[2], str(pageNum)+".txt")
+    with open(fileName, 'w') as f:
+        f.write("title:"+text[0]+"\n")
+        f.write("body:"+text[1]+"\n")
+        try:
+            f.write("next:"+text[2]+"\n")
+        except:
+            print("")
+    
+    imageUrl = imagePath+padNum+".gif"
+    
+    if s.get(imageUrl).status_code == 200:
         urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'.gif')
         
-    elif requests.get(imagePath+padNum+"_1.gif").status_code == 200:
+    elif s.get(imagePath+padNum+"_1.gif").status_code == 200:
         imageUrl = imagePath+padNum+"_1.gif"
         urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'_1.gif')
         imageUrl = imagePath+padNum+"_2.gif"
         urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'_2.gif')
-        
+    
 begin = input("What page to start download on? ")
 end = input("What page to end download on? ")
 current = int(begin)
 total = int(end) - int(begin)
+
+s = requests.Session()
 while not current == int(end)+1:
     fileName = "./downloaded/images/"+str(current).rjust(5,'0')+"*.gif"
+
+    
     if not glob.glob(fileName, recursive=True):
         t = threading.Thread(target=dlPage, args=(current,))
         t.start()
