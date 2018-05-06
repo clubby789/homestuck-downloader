087697e6132bc180a088d2b2e9a57a74e2293041
clubby789 <jamie@hill-daniel.co.uk>
2018-05-06 16:03:49 +0100

    Ensure files download properly
    Files sometimes downloaded as 0 bytes; will now redownload 0-byte files.

--- HomestuckDL.py
+++ HomestuckDL.py
@@ -1,4 +1,4 @@
-import urllib,requests,os,threading,time,glob
+import urllib.request,requests,os,threading,time,glob
 directories=["/downloaded","/downloaded/images"]
 imagePath = "https://www.homestuck.com/images/storyfiles/hs2/"
 
@@ -9,28 +9,30 @@ for i in directories:
 
 def dlPage(pageNum):
     
-    padNum = str(pageNum).rjust(5,'0')
-    if glob.glob(padNum+'*.gif'):
-        return("Already got page "+pageNum)
-        
+    padNum = str(pageNum).rjust(5,'0')       
     url = "https://www.homestuck.com/story/"+str(pageNum)
     imageUrl = imagePath+padNum+".gif"
-    #print(imagepath+pageNum+".gif","downloaded/images/"+padNum+".gif")
-    try:
-        urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'.gif')
-    except:
-        try:
-            urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'_1.gif')
-            urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'_2.gif')
-        except:
-            print("Error on "+padNum)
-            return False
-    return True
 
+    if requests.get(imageUrl).status_code == 200:
+        urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'.gif')
+        
+    elif requests.get(imagePath+padNum+"_1.gif").status_code == 200:
+        imageUrl = imagePath+padNum+"_1.gif"
+        urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'_1.gif')
+        imageUrl = imagePath+padNum+"_2.gif"
+        urllib.request.urlretrieve(imageUrl,'downloaded/images/'+padNum+'_2.gif')
+        
 begin = input("What page to start download on? ")
 end = input("What page to end download on? ")
 current = int(begin)
+total = int(end) - int(begin)
 while not current == int(end)+1:
-    t = threading.Thread(target=dlPage, args=(current,))
-    t.start()
+    fileName = "./downloaded/images/"+str(current).rjust(5,'0')+"*.gif"
+    if not glob.glob(fileName, recursive=True):
+        t = threading.Thread(target=dlPage, args=(current,))
+        t.start()
     current+=1
+for i in glob.glob("./downloaded/images/*.gif"):
+    while os.path.getsize(i) == 0:
+        urllib.request.urlretrieve(imagePath+i[20:],i)
+        
